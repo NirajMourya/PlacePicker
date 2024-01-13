555052491ed01bb267e2a8031156fb548fbe6988
NirajMourya <nirajmourya786@gmail.com>
2024-01-14 01:47:37 +0530

    using useEffect in Modal with dependencies

--- placepicker/src/App.js
+++ placepicker/src/App.js
@@ -14,8 +14,8 @@ const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
    
 function App() {
   
-  const modal = useRef();
   const selectedPlace = useRef();
+  const [modalIsOpen,setModalIsOpen] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([])
   const [pickedPlaces, setPickedPlaces] = useState(storedPlace);
   
@@ -27,12 +27,12 @@ function App() {
   },[])
 
   function handleStartRemovePlace(id) {
-    modal.current.open();
+    setModalIsOpen(true);
     selectedPlace.current = id;
   }
 
   function handleStopRemovePlace() {
-    modal.current.close();
+    setModalIsOpen(false);
   }
 
   function handleSelectPlace(id) {
@@ -52,7 +52,7 @@ function App() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
-    modal.current.close();
+    setModalIsOpen(false);
 
     const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
     localStorage.setItem('selectedPlaces',JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current)))
@@ -60,7 +60,7 @@ function App() {
 
   return (
     <>
-      <Modal ref={modal}>
+      <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
